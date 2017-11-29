a8251b1157ea7c334be8f0ab3218446070baf6b9
Greg Stevenson <greg@stevenson.gs>
2017-11-29 20:12:54 +0000

    Add TP-Link switch, new zone, tidy up a little

--- copy-hass-config.ps1
+++ copy-hass-config.ps1
@@ -1,5 +1,5 @@
 ﻿$from = '\\pi\hass\'
-$to = 'C:\Users\greg\Documents\Projects\ha-config'
+$to = 'C:\git\repos\ha-config'
 
 Get-ChildItem -Path $from | % { 
   Copy-Item $_.fullname "$to" -Recurse -Force -Exclude @("home-assistant.log", "home-assistant_v2.db", "options.xml", "OZW_Log.txt", "plex.conf", "pyozw.sqlite", "YCWT2R~1", "zwcfg_0xe977f2da.xml", "zwscene.xml") 
