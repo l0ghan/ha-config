a3f81823e97122a019aa1ef25bd735727edc8e17
Greg Stevenson <greg@nibley.local>
2017-12-01 13:36:27 +0000

    Add bedroom nodemcu and pi temperature

--- copy-hass-config.ps1
+++ copy-hass-config.ps1
@@ -1,8 +1,8 @@
-﻿$from = '\\pi\hass\'
+﻿$from = '\\192.168.1.65\hass\'
 $to = 'C:\git\repos\ha-config'
 
 Get-ChildItem -Path $from | % { 
   Copy-Item $_.fullname "$to" -Recurse -Force -Exclude @("home-assistant.log", "home-assistant_v2.db", "options.xml", "OZW_Log.txt", "plex.conf", "pyozw.sqlite", "YCWT2R~1", "zwcfg_0xe977f2da.xml", "zwscene.xml") 
 }
 
-Get-Date | Set-Content \\pi\hass\lastbackup.txt
+Get-Date | Set-Content \\192.168.1.65\hass\lastbackup.txt
